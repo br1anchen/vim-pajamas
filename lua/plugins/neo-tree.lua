2cb2a8ab662ee9f1680fff422116bfaa800b2216
Brian Chen <brianchen8990@gmail.com>
2023-02-02 14:51:16 +0100

    feat: add nvim-window-picker

--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -1,8 +1,19 @@
 return {
-  "nvim-neo-tree/neo-tree.nvim",
-  opts = {
-    window = {
-      position = "right",
+  {
+    "s1n7ax/nvim-window-picker",
+    config = function()
+      require("window-picker").setup()
+    end,
+  },
+  {
+    "nvim-neo-tree/neo-tree.nvim",
+    depends = {
+      "s1n7ax/nvim-window-picker",
+    },
+    opts = {
+      window = {
+        position = "right",
+      },
     },
   },
 }
