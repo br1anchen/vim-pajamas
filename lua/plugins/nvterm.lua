4b6676a88cb6804ec8c9d1def15aeb70a27cf242
Brian Chen <brianchen8990@gmail.com>
2023-02-03 16:04:44 +0100

    feat: add nvterm

---
+++ lua/plugins/nvterm.lua
@@ -0,0 +1,78 @@
+return {
+  "NvChad/nvterm",
+  module = "nvterm",
+  keys = {
+    {
+      "<A-i>",
+      function()
+        require("nvterm.terminal").toggle("float")
+      end,
+      mode = "t",
+      desc = "toggle floating term",
+    },
+    {
+      "<A-h>",
+      function()
+        require("nvterm.terminal").toggle("horizontal")
+      end,
+      mode = "t",
+      desc = "toggle horizontal term",
+    },
+    {
+      "<A-v>",
+      function()
+        require("nvterm.terminal").toggle("vertical")
+      end,
+      mode = "t",
+      desc = "toggle vertical term",
+    },
+    {
+      "<A-i>",
+      function()
+        require("nvterm.terminal").toggle("float")
+      end,
+      mode = "n",
+      desc = "toggle floating term",
+    },
+    {
+      "<A-h>",
+      function()
+        require("nvterm.terminal").toggle("horizontal")
+      end,
+      mode = "n",
+      desc = "toggle horizontal term",
+    },
+    {
+      "<A-v>",
+      function()
+        require("nvterm.terminal").toggle("vertical")
+      end,
+      mode = "n",
+      desc = "toggle vertical term",
+    },
+  },
+  config = function()
+    require("nvterm").setup({
+      terminals = {
+        list = {},
+        type_opts = {
+          float = {
+            relative = "editor",
+            row = 0.1,
+            col = 0.25,
+            width = 0.5,
+            height = 0.7,
+            border = "single",
+          },
+          horizontal = { location = "rightbelow", split_ratio = 0.3 },
+          vertical = { location = "rightbelow", split_ratio = 0.5 },
+        },
+      },
+      behavior = {
+        close_on_exit = true,
+        auto_insert = true,
+      },
+      enable_new_mappings = true,
+    })
+  end,
+}
