ad333bae669bb77825f0dc641a2fca7e19d14ba0
Brian Chen <brianchen8990@gmail.com>
2023-02-07 23:00:13 +0100

    feat: add vim-better-whitescape

---
+++ lua/plugins/vim-better-whitespace.lua
@@ -0,0 +1,10 @@
+return {
+  {
+    "ntpeters/vim-better-whitespace",
+    config = function()
+      vim.g.better_whitespace_enabled = 1
+      vim.g.strip_whitespace_on_save = 1
+      vim.g.strip_whitespace_confirm = 0
+    end,
+  },
+}
