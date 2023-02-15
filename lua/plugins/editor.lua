b1a208c8d81397307cc07eb5222395fa926335f0
Brian Chen <brianchen8990@gmail.com>
2023-02-15 15:15:38 +0100

    feat: add git-messenger

--- lua/plugins/editor.lua
+++ lua/plugins/editor.lua
@@ -1,3 +1,14 @@
 return {
   { "tpope/vim-abolish" },
+
+  {
+    "rhysd/git-messenger.vim",
+    config = function()
+      require("which-key").register({
+        g = {
+          m = { "<cmd>GitMessenger<cr>", "git messenger" },
+        },
+      }, { prefix = "<leader>", mode = "n" })
+    end,
+  },
 }
