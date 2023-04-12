0f3f1bee6104be7285d412732773110268657fd5
Brian Chen <brianchen8990@gmail.com>
2023-04-12 10:35:09 +0200

    feat: add smart-splits

--- lua/plugins/editor.lua
+++ lua/plugins/editor.lua
@@ -43,4 +43,96 @@ return {
       require("Comment").setup()
     end,
   },
+
+  {
+    "mrjones2014/smart-splits.nvim",
+    keys = {
+      {
+        "<A-<>",
+        function()
+          require("smart-splits").resize_left()
+        end,
+        desc = "resize_left",
+      },
+      {
+        "<A-+>",
+        function()
+          require("smart-splits").resize_down()
+        end,
+        desc = "resize_down",
+      },
+      {
+        "<A-->",
+        function()
+          require("smart-splits").resize_up()
+        end,
+        desc = "resize_up",
+      },
+      {
+        "<A->>",
+        function()
+          require("smart-splits").resize_right()
+        end,
+        desc = "resize_right",
+      },
+      -- moving between splits
+      {
+        "<C-h>",
+        function()
+          require("smart-splits").move_cursor_left()
+        end,
+        desc = "move_cursor_left",
+      },
+      {
+        "<C-j>",
+        function()
+          require("smart-splits").move_cursor_down()
+        end,
+        desc = "move_cursor_down",
+      },
+      {
+        "<C-k>",
+        function()
+          require("smart-splits").move_cursor_up()
+        end,
+        desc = "move_cursor_up",
+      },
+      {
+        "<C-l>",
+        function()
+          require("smart-splits").move_cursor_right()
+        end,
+        desc = "move_cursor_right",
+      },
+      -- swapping buffers between windows
+      {
+        "<leader><leader>h",
+        function()
+          require("smart-splits").swap_buf_left()
+        end,
+        desc = "swap_buf_left",
+      },
+      {
+        "<leader><leader>j",
+        function()
+          require("smart-splits").swap_buf_down()
+        end,
+        desc = "swap_buf_down",
+      },
+      {
+        "<leader><leader>k",
+        function()
+          require("smart-splits").swap_buf_up()
+        end,
+        desc = "swap_buf_up",
+      },
+      {
+        "<leader><leader>l",
+        function()
+          require("smart-splits").swap_buf_right()
+        end,
+        desc = "swap_buf_right",
+      },
+    },
+  },
 }
