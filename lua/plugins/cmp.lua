c91cc771ff4f4fa34e17cae0545d990c040bb526
Brian Chen <brianchen8990@gmail.com>
2023-05-26 10:05:27 +0200

    refactor: use extras.lang.tailwind instead of custom config

--- lua/plugins/cmp.lua
+++ lua/plugins/cmp.lua
@@ -30,20 +30,9 @@ return {
   },
 
   {
-    "roobert/tailwindcss-colorizer-cmp.nvim",
-    -- optionally, override the default options:
-    config = function()
-      require("tailwindcss-colorizer-cmp").setup({
-        color_square_width = 2,
-      })
-    end,
-  },
-
-  {
     "hrsh7th/nvim-cmp",
     dependencies = {
       { "hrsh7th/cmp-emoji" },
-      { "roobert/tailwindcss-colorizer-cmp.nvim" },
     },
     opts = function(_, opts)
       local has_words_before = function()
@@ -79,14 +68,6 @@ return {
           end
         end, { "i", "s" }),
       })
-
-      -- original LazyVim kind icon formatter
-      local format_kinds = opts.formatting.format
-      opts.formatting.format = function(entry, item)
-        format_kinds(entry, item)
-
-        return require("tailwindcss-colorizer-cmp").formatter(entry, item)
-      end
 
       opts.sources = cmp.config.sources(vim.list_extend(opts.sources, { { name = "emoji" } }))
     end,
