12b74214d7586628e6b47f6ed9c68af53cc62f78
Brian Chen <brianchen8990@gmail.com>
2023-07-12 23:39:24 +0200

    refactor: remove duplicated tsserver config

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -165,21 +165,6 @@ return {
           require("telescope").load_extension("flutter")
           return true
         end,
-        tsserver = function(_, opts)
-          require("lazyvim.util").on_attach(function(client, buffer)
-            if client.name == "tsserver" then
-              vim.keymap.set(
-                "n",
-                "<leader>co",
-                "TypescriptOrganizeImports",
-                { buffer = buffer, desc = "Organize Imports" }
-              )
-              vim.keymap.set("n", "<leader>cR", "TypescriptRenameFile", { desc = "Rename File", buffer = buffer })
-            end
-          end)
-          require("typescript").setup({ server = opts })
-          return true
-        end,
       },
     },
   },
