f33a3a352f8ca0f4109bfc4cf72a19c22c388ee4
Brian Chen <brianchen8990@gmail.com>
2023-07-12 23:38:34 +0200

    refactor: change null-ls config with lazy opts

--- lua/plugins/null-ls.lua
+++ lua/plugins/null-ls.lua
@@ -1,53 +1,54 @@
 return {
   "jose-elias-alvarez/null-ls.nvim",
-  opts = function()
+  opts = function(_, opts)
     local nls = require("null-ls")
     local b = nls.builtins
-    return {
-      sources = {
-        -- JS html css stuff
-        b.formatting.prettier,
-        b.diagnostics.eslint,
 
-        -- Lua
-        b.formatting.stylua,
-        b.diagnostics.luacheck.with({ extra_args = { "--global vim" } }),
+    -- JS html css stuff
+    table.insert(opts.sources, b.formatting.prettier)
+    table.insert(opts.sources, b.diagnostics.eslint)
 
-        -- Shell
-        b.formatting.shfmt,
-        b.diagnostics.shellcheck.with({ diagnostics_format = "#{m} [#{c}]" }),
+    -- Lua
+    table.insert(opts.sources, b.formatting.stylua)
+    table.insert(opts.sources, b.diagnostics.luacheck.with({ extra_args = { "--global vim" } }))
 
-        -- Rust
-        b.formatting.rustfmt,
+    -- Shell
+    table.insert(opts.sources, b.formatting.shfmt)
+    table.insert(opts.sources, b.diagnostics.shellcheck.with({ diagnostics_format = "#{m} [#{c}]" }))
 
-        -- CSS
-        b.diagnostics.stylelint,
-        b.formatting.stylelint,
+    -- Rust
+    table.insert(opts.sources, b.formatting.rustfmt)
 
-        -- Dart
-        b.formatting.dart_format,
+    -- CSS
+    table.insert(opts.sources, b.diagnostics.stylelint)
+    table.insert(opts.sources, b.formatting.stylelint)
 
-        -- Nix
-        b.formatting.nixfmt,
-        b.diagnostics.statix,
+    -- Dart
+    table.insert(opts.sources, b.formatting.dart_format)
 
-        -- Python
-        b.formatting.black,
+    -- Nix
+    table.insert(opts.sources, b.formatting.nixfmt)
+    table.insert(opts.sources, b.diagnostics.statix)
 
-        -- Swift/Obj-c
-        -- b.formatting.swiftformat,
-        b.formatting.swift_format,
+    -- Python
+    table.insert(opts.sources, b.formatting.black)
 
-        -- XML
+    -- Swift/Obj-c
+    -- b.formatting.swiftformat,
+    table.insert(opts.sources, b.formatting.swift_format)
 
-        -- SQL
-        b.formatting.sqlfluff.with({
-          extra_args = { "--dialect", "mysql" }, -- change to your dialect
-        }),
-        b.diagnostics.sqlfluff.with({
-          extra_args = { "--dialect", "mysql" }, -- change to your dialect
-        }),
-      },
-    }
+    -- SQL
+    table.insert(
+      opts.sources,
+      b.formatting.sqlfluff.with({
+        extra_args = { "--dialect", "mysql" }, -- change to your dialect
+      })
+    )
+    table.insert(
+      opts.sources,
+      b.diagnostics.sqlfluff.with({
+        extra_args = { "--dialect", "mysql" }, -- change to your dialect
+      })
+    )
   end,
 }
