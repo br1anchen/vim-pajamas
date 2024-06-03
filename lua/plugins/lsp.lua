fb3e37f930fcd2ab8563b5ab05a1db96c8abd3f7
Brian Chen <brianchen8990@gmail.com>
2024-06-03 09:29:21 +0200

    refactor(typescript): disable inlayHints

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -78,6 +78,47 @@ return {
       servers = {
         dartls = {},
         sourcekit = {},
+        tsserver = {
+          keys = {
+            {
+              "<leader>co",
+              function()
+                vim.lsp.buf.code_action({
+                  apply = true,
+                  context = {
+                    only = { "source.organizeImports.ts" },
+                    diagnostics = {},
+                  },
+                })
+              end,
+              desc = "Organize Imports",
+            },
+            {
+              "<leader>cR",
+              function()
+                vim.lsp.buf.code_action({
+                  apply = true,
+                  context = {
+                    only = { "source.removeUnused.ts" },
+                    diagnostics = {},
+                  },
+                })
+              end,
+              desc = "Remove Unused Imports",
+            },
+          },
+          settings = {
+            typescript = {
+              inlayHints = false,
+            },
+            javascript = {
+              inlayHints = false,
+            },
+            completions = {
+              completeFunctionCalls = true,
+            },
+          },
+        },
         yamlls = {
           settings = {
             yaml = {
