b0808d99347288ecde19a0d4bfeb1c49f57795d5
Brian Chen <brianchen8990@gmail.com>
2024-06-10 09:22:03 +0200

    refactor(lsp): disable ts/js inlayhint

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -82,12 +82,12 @@ return {
           settings = {
             typescript = {
               inlayHints = {
-                enumMemberValues = { enabled = true },
-                functionLikeReturnTypes = { enabled = true },
-                parameterNames = { enabled = "literals" },
+                parameterNames = { enabled = "none" },
                 parameterTypes = { enabled = false },
-                propertyDeclarationTypes = { enabled = true },
                 variableTypes = { enabled = false },
+                propertyDeclarationTypes = { enabled = false },
+                functionLikeReturnTypes = { enabled = false },
+                enumMemberValues = { enabled = false },
               },
             },
           },
